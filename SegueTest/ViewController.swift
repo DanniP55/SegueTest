5955c92d8558c78a9c7c5f3a1dbf76edb86cfb4a
Danni Melton <danni_philpot12@yahoo.com>
2024-09-19 22:04:32 -0400

    SequeTest

--- SegueTest/ViewController.swift
+++ SegueTest/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func Click(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "Manual", sender: self)    }
+    
+   
+    @IBAction func unwind(_ sender: UIStoryboardSegue) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
